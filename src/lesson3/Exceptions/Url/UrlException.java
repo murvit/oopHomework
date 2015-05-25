56563399cb38461071c65ec81b745393fa6e128d
murvit <murvit@ukr.net>
2015-05-25 21:32:40 +0300

    new code for lesson 3

---
+++ src/lesson3/Exceptions/Url/UrlException.java
@@ -0,0 +1,11 @@
+package lesson3.Exceptions.Url;
+
+/**
+ * Created by VMurashkin on 25.05.2015.
+ */
+public class UrlException extends Exception {
+    public UrlException(String message) {
+        super(message);
+    }
+
+}
