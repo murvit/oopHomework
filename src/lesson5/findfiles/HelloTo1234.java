67b74b4bd35adcb59d556beb19483c38d0034a1f
murvit <murvit@ukr.net>
2015-05-27 13:28:01 +0300

    lesson5

--- src/lesson5/findfiles/HelloTo1234.java
+++ src/lesson5/findfiles/HelloTo1234.java
@@ -1,7 +1,30 @@
 package lesson5.findfiles;
 
+import util.Constants;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
- * Created by User on 27.05.2015.
+ * Created by VMurashkin on 27.05.2015.
+ * String Hello to 1234
  */
 public class HelloTo1234 {
+
+    public void changeHello(String file) {
+
+        try (FileReader fr = new FileReader(Constants.FILE_PATH_LESSON_5 + file)) {
+            int i = fr.read(char [] chars);
+            System.out.println("I=" + i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        HelloTo1234 h = new HelloTo1234();
+        h.changeHello("5.txt");
+    }
+
 }
